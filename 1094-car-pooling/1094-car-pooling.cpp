4c76d77bac75e145da330e301d4e35a7596f4981
sakshi kumari <56168294+sakshi-06@users.noreply.github.com>
2022-01-06 11:16:32 +0530

    Time: 55 ms (5.10%), Space: 9.7 MB (95.84%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
 
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        map<int,int> mp;
+        int passengers[1001]={0};
         for(auto &t:trips)
         {
-            mp[t[1]]+=t[0]; //adding the curr passangers at the time
-            mp[t[2]]-=t[0];  // removing the passangers if ther end point is reached
-        }
-        for(auto &x:mp)
-        {
-            if((capacity -= x.second)<0) return false; // at any time we have more passangers than capacity
+            for(int i=t[1];i<t[2];i++)
+            {
+                passengers[i]+=t[0];
+                if(passengers[i]>capacity) return false;
+            }
         }
         return true;
     }
