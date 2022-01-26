214cd58f98dde27ea31c2b0164ff5b8a7678cdd1
sakshi kumari <sakshi6820@gmail.com>
2022-01-26 10:35:13 +0530

    Time: 114 ms (44.11%), Space: 10.9 MB (76.89%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
+    int dp[13][10001];
+    int solve(vector<int>&coins, int amount,int pos)
+    {
+        if(pos==coins.size()) return INT_MAX-1;
+        if(amount==0) return 0;
+        if(dp[pos][amount]!=-1) return dp[pos][amount];
+        int ans=0;
+        if(coins[pos]<=amount)
+        {
+            ans=min(1+solve(coins,amount-coins[pos],pos),solve(coins,amount,pos+1));
+        }
+        else ans=solve(coins,amount,pos+1);
+        return dp[pos][amount]=ans;
+        
+    }
     int coinChange(vector<int>& coins, int amount) {
-        int dp[coins.size()+1][amount+1];
-        for(int i=0;i<=coins.size();i++)
-        {
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=amount;i++)
-        {
-            dp[0][i]=INT_MAX/2;
-        }
-        dp[0][0]=0;
-        for(int i=1;i<=coins.size();i++)
-        {
-            for(int j=1;j<=amount;j++)
-            {
-                if(j-coins[i-1]>=0)
-                {
-                    dp[i][j]=min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        if(dp[coins.size()][amount]==INT_MAX/2) return -1;
-        return dp[coins.size()][amount];
+        memset(dp, -1, sizeof(dp));
+        if(solve(coins,amount,0)==INT_MAX-1) return -1;
+        return solve(coins,amount,0);
+            
     }
 };
