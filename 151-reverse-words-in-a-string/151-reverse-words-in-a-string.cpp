742d380a5e677de378df52aa18fbc69eb1ce3597
sakshi kumari (she/her) <sakshi6820@gmail.com>
2022-02-12 12:24:05 +0530

    Time: 7 ms (54.26%), Space: 7.9 MB (46.11%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        stringstream ss(s);
+        string temp="";
+        while(ss>>temp)
+        {
+            words.push_back(temp);
+        }
+        string ans="";
+        for(int i=words.size()-1;i>=0;i--)
+        {
+            //if(i!=words.size()-1) ans+=" ";
+            ans+=words[i];
+            if(i!=0) ans+=" ";
+        }
+        return ans;
+    }
+};
