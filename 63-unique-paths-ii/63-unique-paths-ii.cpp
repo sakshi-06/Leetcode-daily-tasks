214c97791a859c8579c8f101c8e8c054acd7a1e8
sakshi kumari (she/her) <sakshi6820@gmail.com>
2022-02-14 09:15:09 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (38.23%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int dp[101][101];
-    int solve(int i,int j,vector<vector<int>> &grid)
-    {
-        
-        if(i>=grid.size() || j>=grid[0].size()) return 0;
-        if(grid[i][j]==1) return 0;
-        if(i==grid.size()-1 && j==grid[0].size()-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        ans=solve(i+1,j,grid)+solve(i,j+1,grid);
-        return dp[i][j]=ans;
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0,0,obstacleGrid);
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+       int n=grid.size();
+        int m =grid[0].size();
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        dp[0][0]=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                else if(i==0 && j==0) dp[i][j]=1;
+                else if(i==0)
+                {
+                    dp[i][j]=dp[i][j-1];
+                }
+                else if(j==0)
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
