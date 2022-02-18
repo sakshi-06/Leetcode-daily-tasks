12367fea6f0f1655c99c21b8553b55d8e59cd21d
sakshi kumari (she/her) <sakshi6820@gmail.com>
2022-02-18 09:12:46 +0530

    Time: 7 ms (13.42%), Space: 8.1 MB (99.65%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[high],nums[mid]);
+                high--;
+            }
+        }
+        
+    }
+};
