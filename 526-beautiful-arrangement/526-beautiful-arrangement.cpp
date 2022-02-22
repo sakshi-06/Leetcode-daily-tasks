01b3c7ee95b65f00b08d786b0720421da23b8d46
sakshi kumari (she/her) <sakshi6820@gmail.com>
2022-02-22 10:32:43 +0530

    Time: 68 ms (83.70%), Space: 5.9 MB (82.21%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int res=0;
-    vector<bool> vis;
-    void dfs(int n,int pos)
-    {
-        if(pos>n)
-        {
-            res++;
-        }
+    int seen=0;
+    int countArrangement(int n,int pos=1) {
+        
+        int bit=0;
+        if(pos>n)res++;
         for(int i=1;i<=n;i++)
         {
-            if(!vis[i] &&(pos%i==0 || i%pos==0))
+            bit=1<<i;
+            if(!(seen& bit) && (i%pos==0 || pos%i==0))
             {
-                vis[i]=true;
-                dfs(n,pos+1);
-                vis[i]=false;
+                seen^=bit;
+                countArrangement(n,pos+1);
+                seen^=bit;
             }
         }
-        
-    }
-    int countArrangement(int n) {
-        vis.resize(n+1,false);
-        dfs(n,1);
         return res;
     }
 };
