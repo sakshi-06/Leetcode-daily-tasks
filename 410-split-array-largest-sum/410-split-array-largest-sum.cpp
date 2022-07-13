c27b21aef68fa09012e6523f2f58ece0d1ba9c6d
Sakshi <sakshi6820@gmail.com>
2022-07-13 09:14:07 +0530

    Time: 578 ms (12.05%), Space: 7.5 MB (21.30%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
-    bool check(vector<int> &nums,int cuts,int max_sum)
+    int dp[1001][51];
+    int solve(int i, vector<int> &nums,int n,int m)
     {
-        int sum=0;
-        for(auto &x:nums)
+        if(i==n && m==0) return 0;
+        if(i==n||m==0) return INT_MAX;
+        if(dp[i][m]!=-1) return dp[i][m];
+        int ans=INT_MAX;
+        for(int ind=i;ind<n;ind++)
         {
-            if(x>max_sum)
-            {
-                return false;
-            }
-            else if(x+sum<=max_sum)
-            {
-                sum+=x;
-            }
-            else
-            {
-                cuts--;
-                sum=x;
-                if(cuts<0) return false;
-            }
+            int sum=nums[ind]-((i-1>=0)?nums[i-1]:0);
+            int ff=solve(ind+1,nums,n,m-1);
+            ans=min(ans,max(sum,ff));
         }
-        return true;
+        return dp[i][m]=ans;
     }
     int splitArray(vector<int>& nums, int m) {
-        int l=0,r=0;
-        for(int i=0;i<nums.size();i++)
+        memset(dp, -1, sizeof(dp));
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            r+=nums[i];
+            if(nums[i]==INT_MAX) return INT_MAX;
+            nums[i]+=((i-1>=0)?nums[i-1]:0);
         }
-        int ans=0;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            if(check(nums,m-1,mid)) 
-            {
-                ans=mid;
-                r=mid-1;
-            }
-            else l=mid+1;
-        }
-        return ans;
+        return solve(0,nums,n,m);
     }
 };
