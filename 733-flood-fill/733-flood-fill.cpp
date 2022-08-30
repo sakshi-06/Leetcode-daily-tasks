eba6a56a0979b93d9d92d4077ecf3a77097ea34e
Sakshi <sakshi6820@gmail.com>
2022-08-30 16:51:51 +0530

    Time: 14 ms (61.68%), Space: 13.8 MB (98.75%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &image,int i,int j,int newColor,int curr)
+     void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
     {
-        if(i<0||j<0||i>=image.size()||j>=image[0].size()||image[i][j]!=curr)
+        if(i<0 || i>=image.size() || j<0 || j>= image[0].size() || image[i][j] == newColor || image[i][j] != val)
         {
-            return ;
+            return;
         }
-        image[i][j]=-newColor;
-        dfs(image,i+1,j,newColor,curr);
-        dfs(image,i-1,j,newColor,curr);
-        dfs(image,i,j+1,newColor,curr);
-        dfs(image,i,j-1,newColor,curr);
+        image[i][j] = newColor;
+        dfs(image,i-1,j,val,newColor);
+        dfs(image,i+1,j,val,newColor);
+        dfs(image,i,j-1,val,newColor);
+        dfs(image,i,j+1,val,newColor);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        dfs(image,sr,sc,newColor,image[sr][sc]);
-        for(int i=0;i<image.size();i++)
-        {
-            for(int j=0;j<image[i].size();j++)
-            {
-                if(image[i][j]<0) image[i][j]=-image[i][j];
-            }
-        }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
+    {
+        int val = image[sr][sc];
+        dfs(image,sr,sc,val,newColor);
         return image;
     }
 };
