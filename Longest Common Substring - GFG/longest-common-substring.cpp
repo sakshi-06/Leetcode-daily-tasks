3b27fd948edf0a654b89014c2f2276c802cfb683
Sakshi <sakshi6820@gmail.com>
2022-10-18 19:18:00 +0530

    Added solution - LeetHub

---
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string s1, string s2, int n, int m)
+    {
+        // your code here
+        int dp[n+1][m+1];
+        dp[0][0]=0;
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=m;i++)
+        {
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        return ans;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
