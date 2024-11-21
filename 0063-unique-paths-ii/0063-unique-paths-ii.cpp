9e013f64da4a6dac9beab92da56e2bee2feff1ab
Sakshi <sakshi6820@gmail.com>
2024-11-21 09:07:38 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (82.29%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -6,6 +6,7 @@ public:
         if(x>n-1 || y>m-1 || obstacleGrid[x][y]==1) return 0;
         if(x==n-1 && y==m-1)
             return 1;
+        if(obstacleGrid[x][y] == 1) return dp[x][y]=0;
         if(dp[x][y]!=-1) return dp[x][y];
         return dp[x][y] = solve(obstacleGrid, x+1, y, n, m) + solve(obstacleGrid, x, y+1, n,m);
     }
