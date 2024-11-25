be6a0859c0d18f1aa20d1530bc5423a70f662478
Sakshi <sakshi6820@gmail.com>
2024-11-25 09:55:31 +0530

    Time: 55 ms (54.25%), Space: 49.3 MB (50.07%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -1,65 +1,18 @@
-class Trie {
-public:
-    struct trieNode {
-        bool endofword;
-        trieNode* children[27];
-    };
-
-    trieNode* getNode() {
-        trieNode* node = new trieNode();
-        for (int i = 0; i < 27; i++) {
-            node->children[i] = NULL;
-        }
-        node->endofword = false;
-        return node;
-    }
-
-    trieNode* root;
-    Trie() { root = getNode(); }
-
-    void insert(string word) {
-        trieNode* crawler = root;
-        for (int i = 0; i < word.length(); i++) {
-            int idx = (word[i] == '/') ? 26 : word[i] - 'a';
-            if (crawler->children[idx] == NULL) {
-                crawler->children[idx] = getNode();
-            }
-            crawler = crawler->children[idx];
-        }
-        crawler->endofword = true;
-    }
-
-    bool search(string word) {
-        trieNode* crawler = root;
-        for (int i = 0; i < word.length(); i++) {
-            int idx = (word[i] == '/') ? 26 : word[i] - 'a';
-            if (crawler->children[idx] == NULL) {
-                return false;
-            }
-            crawler = crawler->children[idx];
-            if (crawler->endofword && i < word.length() - 1 && word[i + 1] == '/') {
-                return false;  // Found a subfolder in the trie
-            }
-        }
-        return true;
-    }
-};
-
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        vector<string> res;
-        Trie trie;
-        for (string str : folder) {
-            trie.insert(str);
-        }
-
-        for (string str : folder) {
-            if (trie.search(str)) {
-                res.push_back(str);
+        vector<string>ans;
+        sort(folder.begin(), folder.end());
+        ans.push_back(folder[0]);
+        for(int i=1;i<folder.size();i++){
+            string last_folder = ans.back();
+            last_folder.push_back('/');
+            if(folder[i].substr(0,last_folder.size())==last_folder){
+                continue;
             }
+                ans.push_back(folder[i]);
+            
         }
-
-        return res;
+        return ans;
     }
 };
