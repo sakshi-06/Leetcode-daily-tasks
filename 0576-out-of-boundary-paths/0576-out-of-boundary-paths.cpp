719ad33a3d4112a2f3db9917356bbbf5693e4d3f
Sakshi <sakshi6820@gmail.com>
2024-12-08 15:47:38 +0530

    Time: 3 ms (91.88%), Space: 8.8 MB (68.34%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int mod=1e9+7;
     int dp[51][51][51];
-    int dir[4][4]={{-1,0},{1,0},{0,-1},{0,1}};
-    int solve(int i,int j,int maxmove,int m,int n)
-    {
+    vector<vector<int>> dir ={{0, -1},{0,1},{-1,0},{1,0}};
+    int mod = 1e9+7;
+    int solve(int i, int j, int m, int n, int maxMove){
+        
         if(i<0 || j<0|| i>=m || j>=n)
         {
             return 1;
         }
-        if(maxmove==0) return 0;
-        if(dp[i][j][maxmove]!=-1) return dp[i][j][maxmove];
-        int res=0;  //final result
-       for(auto x:dir) //visit all 4 directions
-       {
-           res+=solve(i+x[0],j+x[1],maxmove-1,m,n); 
-           res%=mod;
-       }
-        return dp[i][j][maxmove]=res%mod;
+        if(maxMove==0) return 0;
+        if(dp[i][j][maxMove]!=-1) return dp[i][j][maxMove];
+        int res =0;
+        for(int d=0;d<dir.size();d++){
+            int n_x = i+dir[d][0];
+            int n_y = j+dir[d][1];
+             res += solve(n_x, n_y, m, n, maxMove-1);
+                res=res%mod;
+        }
+        return dp[i][j][maxMove] = res%mod;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         memset(dp, -1, sizeof(dp));
-        return solve(startRow, startColumn, maxMove,m,n);
+        return solve(startRow, startColumn, m ,n , maxMove);
     }
 };
